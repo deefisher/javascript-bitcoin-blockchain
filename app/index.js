fc9327ab301216c90a8e7ca367b4d95b4544f7b4
Dee Fisher <dekanifisher@yahoo.co.uk>
2021-06-18 12:30:20 +0100

    implemented messageHandler types, broadcast transactions

--- app/index.js
+++ app/index.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const Blockchain = require('../blockchain/index');
 const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet/index');
+const TransactionPool = require('../wallet/transaction-pool');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
-const p2pServer = new P2pServer(bc);
+const wallet = new Wallet();
+const tp = new TransactionPool();
+const p2pServer = new P2pServer(bc, tp);
 
 app.use(express.json());
 
 app.get('/blocks', (req, res) => {
     res.json(bc.chain);
+});
+
+app.get('/transactions', (req, res) => {
+    res.json(tp.transactions);
+});
+
+app.post('/transact', (req, res) => {
+    const { recipient, amount } = req.body;
+    const transaction = wallet.createTransaction(recipient, amount, tp);
+    p2pServer.broadcastTranscaction(transaction);
+    res.redirect('/transactions');
 });
 
 app.post('/mine', (req, res) => {
@@ -21,6 +36,10 @@ app.post('/mine', (req, res) => {
     p2pServer.syncChains();
 
     res.redirect('/blocks');
+});
+
+app.get('/public-key', (req, res) => {
+    res.json({ publicKey: wallet.publicKey });
 });
 
 app.listen(HTTP_PORT, () => console.log(`listening on port ${HTTP_PORT}`));
